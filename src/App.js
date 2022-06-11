ec0e6efdbe9fa9d9d247d279c9c7412fa05a96e6
Katelyn Suhr <katy@suhr.com>
2022-06-10 20:10:11 -0700

    Implement 3 column gallery with by fetching images from unsplash

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import ImagesSet from './ImagesSet';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Dog Gallery</h1>
+        <ImagesSet/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ImagesSet.js
@@ -0,0 +1,106 @@
+import React, {useEffect, useState} from 'react';
+
+import {createApi} from 'unsplash-js';
+
+const unsplash = createApi({ accessKey: `${process.env.REACT_APP_ACCESSKEY}` });
+
+
+const columnStyle = {
+    margin: '10px',
+    width: '33.3333%',
+}
+
+const imageStyle = {
+    borderRadius: '5px',
+    width:'100%',
+    marginTop:'10px',
+    fontSize:'20px'
+}
+
+const buttonStyle = {
+    color: 'white',
+    backgroundColor: 'grey',
+    fontSize: '16px',
+    margin: '20px',
+    width: '300px',
+    height: '40px',
+    border: 'none',
+    borderRadius: '5px',
+    textTransform: 'uppercase'
+}
+
+const ImagesSet = () => {
+    const [colA, setAPhotos] = useState([]);
+    const [colB, setBPhotos] = useState([]);
+    const [colC, setCPhotos] = useState([]);
+    const [pageNumber, setPageNumber] = useState(0);
+
+    var colAHeight = 0;
+    var colBHeight = 0;
+    var colCHeight = 0;
+
+    const fetchImages = async (pageNumber) => {
+        setPageNumber(pageNumber);
+        unsplash.search.getPhotos({
+            query:'dogs',
+            perPage: '10',
+            page: pageNumber
+        }).then(result => {
+            const imageArray = result.response.results;
+            imageArray.forEach((img) => {
+                const width = img.width;
+                const height = img.height;
+                const aspectRatio = height/width;
+           
+                if(colAHeight <= colBHeight && colAHeight <= colCHeight) {
+                    colAHeight+=aspectRatio;
+                    setAPhotos(colA => [...colA, img])
+                } else if(colBHeight < colAHeight && colBHeight < colCHeight) {
+                    colBHeight+=aspectRatio;
+                    setBPhotos(colB => [...colB, img])
+                 }else { // colC Height is shorter
+                    colCHeight+=aspectRatio;
+                    setCPhotos(colC => [...colC, img])
+                }
+            });
+        }).catch(err => {
+            console.log(err);
+        })
+        
+    }
+    useEffect(() => {
+        fetchImages(pageNumber);
+    }, []);
+
+    return(
+        <div>
+        <div style={{display:'flex'}}>
+            <div style={columnStyle}>
+                {colA.map((img, i) => {
+                    return(
+                        <img key={i} style={imageStyle} src={img.urls.regular}/>
+                    )
+                })}
+            </div>
+            <div style={columnStyle}>
+                {colB.map((img, i) => {
+                    return(
+                        <img key={i} style={imageStyle} src={img.urls.regular}/>
+                    )
+                })}
+            </div>
+            <div style={columnStyle}>
+                {colC.map((img, i) => {
+                    return(
+                        <img key={i} style={imageStyle} src={img.urls.regular}/>
+                    )
+                })}
+            </div>
+        </div>
+        <button style={buttonStyle} onClick={() => fetchImages(pageNumber+2)}>Load dogs...</button>
+        </div>
+    )
+}
+
+export default ImagesSet;
+
